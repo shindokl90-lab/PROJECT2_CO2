52dfce5b2630db1d3e0dd6b302d2a7963cca1a1a
Greg Behnke <gregbehnke24x7@msn.com>
2021-10-27 18:21:43 -0500

    commit progress

--- CO2_app.py
+++ CO2_app.py
@@ -1,45 +1,28 @@
-# import necessary libraries
-from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import scrape_mars
+# imports
+from flask import Flask, jsonify, request, render_template, redirect
 
 # create instance of Flask app
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+# Use flask to set up db connection
 
 # Create route that renders index.html template and finds documents from mongo
 @app.route("/")
 def home():
-    # see if collection exists yet
-    mars_data = mongo.db.mars.find()
-    # check to see if any results are returned
-    ret = list(mars_data)
-    # If the collection was not found, do the scrape and upsert
-    if len(ret)==0:
-        mars_data = scrape_mars.scrape()
-    else:
-        mars_data = mongo.db.mars.find_one()
+    example_embed='This string is from python'
+    return render_template('index.html', embed=example_embed)
 
-    # Return template and data
-    return render_template("index.html", mars_data=mars_data)
-
-# Route that will trigger scrape function
-@app.route("/scrape")
-def scrape():
-    # Do the scraping
-    mars_data = scrape_mars.scrape()   
-
-    # Update the mongo database 
-    mongo.db.mars.update(
-             {},
-             mars_data,
-             upsert=True
-             )
-    
-    # Redirect back to home page
-    return redirect("/", code=302)
+# Test route
+@app.route('/test', methods=['GET', 'POST'])
+def testfn():
+    # GET request
+    if request.method == 'GET':
+        message = {'greeting':'Hello from Flask!'}
+        return jsonify(message)  # serialize and use JSON headers
+    # POST request
+    if request.method == 'POST':
+        print(request.get_json())  # parse as JSON
+        return 'Sucesss', 200
 
 
 if __name__ == "__main__":
